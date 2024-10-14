001d6790bc704f7d7b4303ee540c378694df2507
AmericaJazminEstradaHernandez <152317829+AmericaJazminEstradaHernandez@users.noreply.github.com>
2024-10-14 07:53:53 -0600

    Version01

--- index.php
+++ index.php
@@ -6,14 +6,20 @@
 <body>
 
 <?php
-    if (isset($_POST['nombre'])) {
+    if (isset($_POST['nombre']) && isset($_POST['fecha'])) {
         $nombre = htmlspecialchars($_POST['nombre']);
+        $fecha = htmlspecialchars($_POST['fecha']);
         echo "<h1>¡Bienvenido, $nombre!</h1>";
+        echo "<p>Has ingresado la fecha: $fecha</p>";
     } else {
 ?>
         <form method="post" action="">
             <label for="nombre">Ingresa tu nombre:</label>
-            <input type="text" id="nombre" name="nombre" required>
+            <input type="text" id="nombre" name="nombre" required><br><br>
+
+            <label for="fecha">Ingresa la fecha:</label>
+            <input type="date" id="fecha" name="fecha" required><br><br>
+
             <input type="submit" value="Enviar">
         </form>
 <?php
@@ -22,3 +28,4 @@
 
 </body>
 </html>
+
